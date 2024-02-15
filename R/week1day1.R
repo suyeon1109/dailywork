c33187adbc7414ded760401bdb9d373b4314f74a
Suyeon Kim <ksy11090@naver.com>
2024-02-15 09:15:38 -0500

    Create week1day1.R

---
+++ R/week1day1.R
@@ -0,0 +1,37 @@
+install.packages("rafalib")
+library(rafalib)
+
+install.packages("swirl")
+library(swirl)
+
+# R.Version()
+
+x <- c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
+sum <- 0
+for (var in x) {
+  sum <- sum + var
+}
+sum/length(x)
+
+
+sum <- 0
+for (i in 1:25) {
+  sum <- sum + (i * i)
+}
+sum
+
+class(cars)
+sum <- 0
+dist <- cars[, 2]
+for (var in dist) {
+  sum <- sum + var
+}
+sum / length(dist)
+
+which(dist == 85)
+
+install.packages("downloader")
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv"
+download(url, destfile = filename)
